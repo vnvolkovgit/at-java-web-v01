89f20c47b46e5accd16a3aeb75e104e8b06abbaf
Sergey Slesarev <slqamsk@gmail.com>
2025-12-14 11:02:40 +0300

    add simplexpathtests.java

--- src/test/java/demo/part03/SimpleXPathTests.java
+++ src/test/java/demo/part03/SimpleXPathTests.java
@@ -1,6 +1,8 @@
 package demo.part03;
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +11,12 @@ import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class SimpleXPathTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadTimeout = 30_000;
+        Configuration.pageLoadStrategy = "eager";
+    }
+
     @BeforeEach
     void openPage () {
         open("https://slqamsk.github.io/tmp/xPath01.html");
@@ -90,5 +98,25 @@ public class SimpleXPathTests {
         System.out.println("Его родитель: тег: " + se2.getTagName() + ", текст: " + se2.text());
         SelenideElement se3 = $x("//h3[.='Маргарита']/../button");
         System.out.println("Кнопка: тег: " + se3.getTagName() + ", текст: " + se3.text());
+    }
+
+    @Test
+    void testHWspecialist() {
+        open("https://www.specialist.ru/");
+        sleep(2_000);
+        $x("//button[@id='cookieConsent__ok']").click();
+        sleep(2_000);
+        $x("//a[.='Курсы']").click();
+        sleep(2_000);
+        $x("//a[.='Каталог курсов']").click();
+        sleep(10_000);
+
+        //$("#CourseName").sendKeys("тестирование");
+        $x("//input[@id='CourseName']").sendKeys("тестирование");
+        $x("//button[@type='submit']").click();
+
+        String myXPath = "//*[contains(text(),'Автоматизированное тестирование веб-приложений с использованием Selenium')]/ancestor::article//dd[contains(@class,'date-start') and contains(@class,'date')]";
+        SelenideElement se = $x(myXPath);
+        se.shouldHave(text("24.01.2026"));
     }
 }
